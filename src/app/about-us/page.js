a08f0506451bca18f78043def94a04e134e12906
Rada Karanyakorn <ellie@Ellie-Jeans.local>
2025-09-18 09:41:15 +0700

    first commit

---
+++ src/app/about-us/page.js
@@ -0,0 +1,12 @@
+import Navbar from "@/Components/Navbar";
+import AboutUsSection from "@/Components/AboutUsSection";
+export default function AboutUs(){
+    return (
+        <div> 
+            <Navbar/> 
+            <AboutUsSection/>
+             </div>
+             
+            
+    )
+}
